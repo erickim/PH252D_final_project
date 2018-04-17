9a5b7089471389a6e68e781a63eb34a5f50f48e1
James Duncan <jpduncan@berkeley.edu>
2018-04-17 13:13:56 -0700

    Log transform net worth and remove duplicate net worth var

--- code/clean.R
+++ code/clean.R
@@ -37,9 +37,8 @@ clean <- data %>%
   filter(ORIG_REAS_ENTITLE_CD == 0) %>%
   select(Vaccination_A = RX_THER_17_YR2015,
          Hospitalization_Y = ADMISSIONS,
-         Net_Worth_W1 = Est_Net_worth,
          Above_Pvt_Line_W1 = Pct_above_poverty_line,
-         Est_Net_Worth_W1 = Est_Net_worth,
+         Est_Net_Worth_W1 = log(Est_Net_worth),
          Education_W2 = Education_level,
          College_W2 = College,
          Age_W3 = AGE,
