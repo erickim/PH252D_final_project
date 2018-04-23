6bd14aa96cbcee5808b04c4d8156e10a5e5ea6be
James Duncan <jpduncan@berkeley.edu>
2018-04-22 20:14:28 -0700

    Remove EDUCATION_LEVEL

--- code/clean.R
+++ code/clean.R
@@ -4,15 +4,14 @@
 #                                                                             #
 ###############################################################################
 
-
-
 #"""""""""""""""""""""""""""#
 # load in data and packages #
 #"""""""""""""""""""""""""""#
 
-data <- read.csv("data/TAMU_FINAL_SUBSET.csv")
-
 library(tidyverse)
+library(readr)
+
+data <- read_csv("data/TAMU_FINAL_SUBSET.csv")
 
 #""""""""""#
 # cleaning #
@@ -24,8 +23,8 @@ library(tidyverse)
 #     [ADMISSIONS]
 # W1 = Income, above/below federal poverty line
 #      [EST_INCOME, PCT_ABOVE_POVERTY_LINE, PCT_BELOW_POVERTY_LINE]
-# W2 = Education, college educated or above/less than college
-#      [EDUCATION_LEVEL, COLLEGE]
+# W2 = college educated or above/less than college
+#      [COLLEGE]
 # W3 = Age
 #      [AGE]
 # W4 = Medical risk score
@@ -40,7 +39,6 @@ clean <- data %>%
          Above_Pvt_Line_W1 = Pct_above_poverty_line,
          Est_income_W1 = Est_income,
          Est_Net_Worth_W1 = Est_Net_worth,
-         Education_W2 = Education_level,
          College_W2 = College,
          Age_W3 = AGE,
          Medical_Risk_W4 = RECON_MA_RISK_SCORE_NBR,
